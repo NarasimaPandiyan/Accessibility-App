70ffc79f48b3ce6a22ded9f056f0079135b80a24
Narasima Pandiyan <35830653+NarasimaPandiyan@users.noreply.github.com>
2023-04-10 01:45:00 +0530

    Ported Android

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'codes.narasima.app',
+  appName: 'Accessibility App',
+  webDir: 'dist/accessibility_app',
+  bundledWebRuntime: true
+};
+
+export default config;
